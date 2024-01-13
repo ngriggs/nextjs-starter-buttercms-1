bd0f88e8b867e74b158e10e48d38044b17f9cbce
Noah Griggs <noahg@vt.edu>
2024-01-13 16:16:55 -0600

    needed usecallback function for useeffect...

--- app/example2/landingPageSection.tsx
+++ app/example2/landingPageSection.tsx
@@ -4,15 +4,26 @@ import TwoColumnWithImage from "./twoColumnWithImage";
 import Features from "./features";
 import Testimonials from "./testimonials";
 
-export default function LandingPageSection({ type, sectionData }) {
-	const sectionsComponentPaths = {
-		hero: Hero,
-		two_column_with_image: TwoColumnWithImage,
-		features: Features,
-		testimonials: Testimonials,
-	};
+type SectionType =
+	| "hero"
+	| "two_column_with_image"
+	| "features"
+	| "testimonials";
 
-	const SectionComponent = sectionsComponentPaths[type] || MissingSection;
+const sectionsComponentPaths: Record<SectionType, React.ComponentType> = {
+	hero: Hero,
+	two_column_with_image: TwoColumnWithImage,
+	features: Features,
+	testimonials: Testimonials,
+};
+export default function LandingPageSection({
+	type,
+	sectionData,
+}: {
+	type: SectionType;
+	sectionData: any;
+}) {
+	const SectionComponent = sectionsComponentPaths[type];
 
 	// Use optional chaining to avoid errors when sectionData is undefined
 	return <SectionComponent type={type} {...sectionData} />;
--- app/example2/typewriter.tsx
+++ app/example2/typewriter.tsx
@@ -1,32 +1,34 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 export default function Typewriter({ texts, classNames }: any) {
 	const [typingText, setTypingText] = useState("");
 
-	const typeText = (index: number, erase: boolean) => {
-		let i = erase ? texts[index].length : 0;
-		const step = erase ? -1 : 1;
+	const typeText = useCallback(
+		(index: number, erase: boolean) => {
+			let i = erase ? texts[index].length : 0;
+			const step = erase ? -1 : 1;
 
-		const intervalId = setInterval(() => {
-			if ((erase && i >= 0) || (!erase && i <= texts[index].length)) {
-				const currentText = texts[index].substring(0, i);
-				setTypingText(currentText);
-				i += step;
-			} else {
-				clearInterval(intervalId);
+			const intervalId = setInterval(() => {
+				if ((erase && i >= 0) || (!erase && i <= texts[index].length)) {
+					const currentText = texts[index].substring(0, i);
+					setTypingText(currentText);
+					i += step;
+				} else {
+					clearInterval(intervalId);
 
-				// Switch between typing and erasing after each full cycle
-				setTimeout(() => {
-					if (!erase) {
-						typeText(index, !erase);
-					} else {
-						typeText((index + 1) % texts.length, !erase);
-					}
-				}, 1000);
-			}
-		}, 100);
-	};
+					setTimeout(() => {
+						if (!erase) {
+							typeText(index, !erase);
+						} else {
+							typeText((index + 1) % texts.length, !erase);
+						}
+					}, 1000);
+				}
+			}, 100);
+		},
+		[texts]
+	);
 
 	useEffect(() => {
 		typeText(0, false);
